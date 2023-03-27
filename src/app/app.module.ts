f7860fb30aa8425766554554dcf39e18dd8b9e57
hugoIbzn <hugo.bazan@nomitek.com.mx>
2023-03-27 13:41:42 -0600

    Se crea el componente gestor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GestorComponent } from './gestor/gestor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GestorComponent
   ],
   imports: [
     BrowserModule,
