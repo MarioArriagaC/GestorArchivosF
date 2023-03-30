0c7d00fcccebb9f57fd912a137c2511d1292d737
MarioArriagaC <mario.arriaga@nomitek.com.mx>
2023-03-30 13:47:28 -0600

    agregar guardar al subir archivo

--- src/app/gestor/gestor.component.ts
+++ src/app/gestor/gestor.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ArchivosService } from '../archivos.service';
+import { formatDate } from '@angular/common';
 @Component({
   selector: 'app-gestor',
   templateUrl: './gestor.component.html',
@@ -17,13 +18,7 @@ export class GestorComponent {
       
     });
 
-    this.ArchivoService.uploadFile({
-      name: "morpheus.png",
-      ruta_archivo: "./assets/files/",
-      status: true
-    }).subscribe(data => {
-      console.log(data);
-    });
+    
 
     this.ArchivoService.editFile({
       id: 2,
@@ -31,7 +26,21 @@ export class GestorComponent {
       ruta_archivo: "./assets/files/",
       status: true
     }).subscribe(data => {
-      console.log(data)
+      //console.log(data)
+    });
+  }
+
+ 
+
+  upload(name: any){
+    const file:File = name.target.files[0];
+    console.log(file)
+    this.ArchivoService.uploadFile({
+      name: file.name,
+      ruta_archivo: "./assets/files/",
+      status: true
+    }).subscribe(data => {
+      
     });
   }
 
