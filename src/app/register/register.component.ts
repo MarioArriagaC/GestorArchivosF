99c12083a68b456715320f9017fc0a178dbbd352
hugoIbzn <hugo.bazan@nomitek.com.mx>
2023-04-06 10:24:03 -0600

    Ya no se puede alterar el estado del usuario

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { RegistrerService } from '../registrer.service';
 import { Router } from '@angular/router';
 import * as CryptoJS from 'crypto-js';
@@ -7,17 +7,32 @@ import * as CryptoJS from 'crypto-js';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
   name = '';
   lastName = '';
   dateBirth = '';
   user = '';
   password = '';
   registrado = false;
-  constructor(private RegisterService: RegistrerService, private router: Router) { }
+  
+  // Variable para obtener el elemento del DOM
+  private txtIpt!: HTMLElement;
+
+  constructor(private RegisterService: RegistrerService, private router: Router, private ipt: ElementRef) { }
+
+  ngOnInit(): void {
+    this.txtIpt = this.ipt.nativeElement.querySelector('.iptUsr');  
+  }
 
   userName() {
     this.user = this.name.substring(0, 3).toLocaleLowerCase() + this.lastName.substring(0, 3).toLocaleLowerCase();
+  }
+
+
+  disable() {
+    alert("Suelta ahí pndejo")
+    this.router.navigate(['/register'])
+    this.txtIpt.setAttribute("disabled", "")
   }
 
   onSubmit() {
@@ -33,17 +48,17 @@ export class RegisterComponent {
         alert("Llena todo, pndejo idiota, baboso, mal parido, hijo de prra, culo si abres las dev tools");
       }
       else if (this.registrado == false) {
-          this.RegisterService.registerUser({
-            Name: this.name,
-            Second_Name: this.lastName,
-            date_birth: this.dateBirth,
-            User: this.user,
-            Password: CryptoJS.SHA256(this.password).toString()
-          }).subscribe(data => {
-            alert('usuario creado con exito');
-            this.router.navigate(['/login']);
-          });
-        }
+        this.RegisterService.registerUser({
+          Name: this.name,
+          Second_Name: this.lastName,
+          date_birth: this.dateBirth,
+          User: this.user,
+          Password: CryptoJS.SHA256(this.password).toString()
+        }).subscribe(data => {
+          alert('usuario creado con exito');
+          this.router.navigate(['/login']);
+        });
+      }
     });
   }
 
